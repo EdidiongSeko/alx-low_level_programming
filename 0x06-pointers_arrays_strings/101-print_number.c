bd7b2ecaa144136490fad0abc9edfec556879b4f
Ekoedidiong <edybless98@gmail.com>
2022-07-15 18:40:59 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_number - prints an integer
  * @n: integer to be printed
@@ -15,6 +16,7 @@ void print_number(int n)
 	}
 	if ((num / 10) > 0)
 		print_number(num / 10);
+
 	_putchar((num % 10) + '0');
 }
 
